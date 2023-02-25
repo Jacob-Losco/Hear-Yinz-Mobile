ba94483d82be2522b543b8460010a6491872a505
firehydrangea <57843561+firehydrangea@users.noreply.github.com>
2023-02-25 14:07:15 -0500

    Layout

--- Hear-Yinz/Hear-Yinz/Views/SettingsView.swift
+++ Hear-Yinz/Hear-Yinz/Views/SettingsView.swift
@@ -8,6 +8,7 @@ Exported Data Structures: SettingsView - the view itself
 Exported Functions: None
 
 Contributors:
+    Sarah Kudrick - 2/25/23 - SP-255
     Jacob Losco - 2/4/2023 - SP-220
 
 ===================================================================+*/
@@ -19,8 +20,8 @@ struct SettingsView: View {
 
     var body: some View {
         VStack{
-            Text("Settings Page")
-            Spacer()
+            //Text("Settings Page")
+            //Spacer()
             Button {
                 oLoginFunctions.fnLogout()
             } label: {
@@ -31,13 +32,21 @@ struct SettingsView: View {
                         .shadow(radius: 10)
                         .padding()
                     Text("Log out")
-                        .font(.title)
+                        .font(.custom("DMSans-Regular", size: 36))
                         .fontWeight(.semibold)
                         .foregroundColor(Color("highlight"))
                 }
                 .padding()
             }
             Spacer()
+            Text("Blocked Organizations")
+                .font(.custom("DMSans-Regular", size: 24))
+            UnblockRowView()
+            UnblockRowView()
+            UnblockRowView()
+            
+            Spacer()
+          
             Rectangle() //Adds custom color background to tab bar.
                 .fill(Color.clear)
                 .frame(height: 10)
@@ -50,5 +59,26 @@ struct SettingsView: View {
 struct SettingsView_Previews: PreviewProvider {
     static var previews: some View {
         SettingsView()
+    }
+}
+
+struct UnblockRowView: View {
+    var body: some View {
+        HStack{
+            Text("Organization name")
+                .font(.custom("DMSans-Regular", size: 18))
+                .padding()
+            Button{
+                
+            } label: {
+                ZStack{
+                    RoundedRectangle(cornerRadius: 10)
+                        .fill(Color("selected"))
+                        .frame(height: 40)
+                    Text("Unblock")
+                        .font(.custom("DMSans-Regular", size: 18))
+                }
+            }
+        }
     }
 }
