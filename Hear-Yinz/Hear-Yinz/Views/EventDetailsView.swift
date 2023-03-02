bc5a7299ce99b0e1a4140d709b2f0282cf9c96cd
Keaton-Hollobaugh <keatonhollobaugh@gmail.com>
2023-03-02 16:01:50 -0500

    Like Button Functionality

--- Hear-Yinz/Hear-Yinz/Views/EventDetailsView.swift
+++ Hear-Yinz/Hear-Yinz/Views/EventDetailsView.swift
@@ -15,21 +15,33 @@ import SwiftUI
 
 struct EventDetailsView: View {
     @Environment(\.presentationMode) var presentationMode
-    
+    @StateObject var oDBFunctions = DBFunctions()
+    @State private var likes = 0 // Reactive state variable for the number of likes
+    @State private var isButtonDisabled = false // Reactive state variable for button disabled state
     var event: EventModel
-    
+
     var body: some View {
         VStack(spacing: 10) {
             Text(event.sm_Name)
                 .font(.title)
                 .font(.custom("DMSans-Regular", size: 18))
-            Button(action: {
-                // Add your upvote action here
-            }) {
-                Image(systemName: "arrow.up.circle")
-                    .resizable()
-                    .frame(width: 30, height: 30)
-                    .foregroundColor(Color(red: 60/255, green: 120/255, blue: 216/255))
+            HStack {
+                Button(action: {
+                    oDBFunctions.fnUpdateEventLikes(sEvent: event) { success in
+                        if success {
+                            likes += 1 // Update the likes reactive state variable
+                            isButtonDisabled = true // Disable the button after it's been clicked
+                        }
+                    }
+                }) {
+                    Image(systemName: "arrow.up.circle")
+                        .resizable()
+                        .frame(width: 30, height: 30)
+                        .foregroundColor(Color(red: 60/255, green: 120/255, blue: 216/255))
+                }
+                .disabled(isButtonDisabled) // Disable the button if isButtonDisabled is true
+                Text("\(likes)") // Display the likes reactive state variable
+                    .font(.custom("DMSans-Regular", size: 18))
             }
             Text(event.sm_LocationName)
                 .font(.custom("DMSans-Regular", size: 18))
@@ -44,6 +56,9 @@ struct EventDetailsView: View {
             Text(event.sm_HostName)
                 .font(.custom("DMSans-Regular", size: 18))
         }
+        .task {
+            await oDBFunctions.fnInitSessionData()
+        }
         .gesture(
             DragGesture()
                 .onEnded { value in
@@ -54,4 +69,3 @@ struct EventDetailsView: View {
         )
     }
 }
-
