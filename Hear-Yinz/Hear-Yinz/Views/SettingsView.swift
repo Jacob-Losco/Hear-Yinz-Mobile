ca1825e0030f4caa279b06e7f6ab14ea65dba5f2
firehydrangea <57843561+firehydrangea@users.noreply.github.com>
2023-03-02 15:00:27 -0500

    errors

--- Hear-Yinz/Hear-Yinz/Views/SettingsView.swift
+++ Hear-Yinz/Hear-Yinz/Views/SettingsView.swift
@@ -1,22 +1,34 @@
 /*+===================================================================
 File: SettingsView
 
+
 Summary: View for settings screen
+
 
 Exported Data Structures: SettingsView - the view itself
 
+
 Exported Functions: None
+
 
 Contributors:
     Sarah Kudrick - 2/25/23 - SP-255
     Jacob Losco - 2/4/2023 - SP-220
 
+
 ===================================================================+*/
+
 
 import SwiftUI
 
+
 struct SettingsView: View {
     @ObservedObject var oLoginFunctions = LoginFunctions() //contains login functions
+    @ObservedObject var oDBFunctions = DBFunctions() //contains login functions
+    //oDBFunctions.fnInitSessionData()
+    //oDBFunctions.fnGetBlockedOrganizations()
+    @State private var aoBlockedList: [OrganizationModel] = []
+
 
     var body: some View {
         VStack{
@@ -41,9 +53,12 @@ struct SettingsView: View {
             //Spacer()
             Text("Blocked Organizations")
                 .font(.custom("DMSans-Regular", size: 24))
-            UnblockRowView()
-            UnblockRowView()
-            UnblockRowView()
+            //UnblockRowView()
+            //UnblockRowView()
+            //UnblockRowView()
+            List(aoBlockedList, id: \.sm_Id){ org in
+                UnblockRowView(oOrganization: org, id: org.sm_Id, name: org.sm_Name)
+            }
             
             Spacer()
           
@@ -52,9 +67,15 @@ struct SettingsView: View {
                 .frame(height: 10)
                 .background(Color("highlight"))
 
-        }
+
+        }.task{
+            await oDBFunctions.fnInitSessionData()
+            await oDBFunctions.fnGetBlockedOrganizations()
+            aoBlockedList = oDBFunctions.aoOrganizationList
+            }
     }
 }
+
 
 struct SettingsView_Previews: PreviewProvider {
     static var previews: some View {
@@ -62,26 +83,5 @@ struct SettingsView_Previews: PreviewProvider {
     }
 }
 
-struct UnblockRowView: View {
-    var body: some View {
-        HStack{
-            Text("Organization name")
-                .font(.custom("DMSans-Regular", size: 18))
-                .padding()
-            Button{
-                
-            } label: {
-                ZStack{
-                    RoundedRectangle(cornerRadius: 10)
-                        .fill(Color("button"))
-                        .frame(height: 40)
-                        .padding(.trailing)
-                    Text("Unblock")
-                        .font(.custom("DMSans-Regular", size: 18))
-                        .foregroundColor(Color.white)
-                        
-                }
-            }
-        }
-    }
-}
+
+
---
+++ Hear-Yinz/Hear-Yinz/Views/UnblockRowView.swift
@@ -0,0 +1,75 @@
+/*+===================================================================
+File: UnblockRowView
+
+
+Summary: View for listing blocked organizations in settings screen
+
+
+Exported Data Structures: UnblockRowView - the view itself
+
+
+Exported Functions: None
+
+
+Contributors:
+    Sarah Kudrick - 2/25/23 - SP-255
+    Jacob Losco - 2/4/2023 - SP-220
+
+
+===================================================================+*/
+
+
+import SwiftUI
+
+
+struct UnblockRowView: View {
+    @ObservedObject var oDBFunctions = DBFunctions() //contains login functions
+    var oOrganization: OrganizationModel
+    @State private var cTapped = Color("button")
+    var body: some View {
+        HStack{
+            Text(oOrganization.sm_Id)
+                .font(.custom("DMSans-Regular", size: 18))
+                .multilineTextAlignment(.center)
+                .padding()
+            Button{
+                cTapped=Color.gray
+                oDBFunctions.fnDeleteRelationshipOrganization(sOrganizationId: oOrganization.sm_Id, iRelationshipType: 2, hCompletionHandler: {(success)->Void in
+                    if success {
+                        cTapped=Color.gray
+                    } else {
+                        print("Error at fnDeleteRelationshipOrganization in UnblockRowView")
+                    }
+                    })//{(result) in}
+                //await oDBFunctions.fnDeleteRelationshipOrganization(sOrganizationId: oOrganization.sm_Id, iRelationshipType: 2){(result) in}
+                //how do i do the bool -> void part
+                //adding {(result) in} is a solution stolen from the internet
+            } label: {
+                ZStack{
+                    RoundedRectangle(cornerRadius: 10)
+                        .fill(cTapped)
+                        .frame(height: 40)
+                        .padding(.trailing)
+                    Text("Unblock")
+                        .font(.custom("DMSans-Regular", size: 18))
+                        .foregroundColor(Color.white)
+                        .multilineTextAlignment(.center)
+                        .frame(height: 40, alignment: .center)
+                        
+                }
+            }
+        }
+    }
+    init(oOrganization: OrganizationModel, id: String, name: String) {
+        self.oOrganization = OrganizationModel(sId: id, sName: name)
+    }
+
+
+}
+
+
+struct UnblockRowView_Previews: PreviewProvider {
+    static var previews: some View {
+        UnblockRowView(oOrganization: OrganizationModel(sId: "test", sName: "test"), id: "test", name: "test")
+    }
+}
